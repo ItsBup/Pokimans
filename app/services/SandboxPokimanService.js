d1a3e61634d69648d7ffdad906f59bd6e4303d3a
Kaden <kjrigsbee@gmail.com>
2023-12-13 14:44:42 -0700

    im tired of this grandpa

--- app/services/SandboxPokimanService.js
+++ app/services/SandboxPokimanService.js
@@ -1,9 +1,6 @@
 import { AppState } from '../AppState.js'
-import { Account } from '../models/Account.js'
-import { logger } from '../utils/Logger.js'
-import { api } from './AxiosService.js'
 
-export const pokeApi = new axios.create ({
+const pokeApi = new axios.create ({
   baseURL: 'https://pokeapi.co/api/v2/pokemon',
   timeout: 8000
 })
